d456316a4da3a31bfe928dd3424b8a7bc5f3a5ad
Loïc <loic@jeedom.com>
2020-05-15 11:57:24 +0200

    fix cookie

--- js/init.js
+++ js/init.js
@@ -351,7 +351,7 @@ function setCookie(cname, cvalue, exdays) {
   var d = new Date();
   d.setTime(d.getTime() + (exdays*24*60*60*1000));
   var expires = "expires="+ d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;samesite=Strict;secure";
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;samesite=Lax;secure";
 }
 
 function cookiesPolicyBar(){
